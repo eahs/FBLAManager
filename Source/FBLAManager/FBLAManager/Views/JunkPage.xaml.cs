b9181088897059b98ffa8dfa2df1dfaeb1c738fe
Makenna Swartz <makennaswartz03@gmail.com>
2019-09-24 12:59:26 -0400

    Create Flyout menu

---
+++ Source/FBLAManager/FBLAManager/Views/JunkPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FBLAManager.Views
+{
+    public partial class JunkPage : ContentPage
+    {
+        public JunkPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
