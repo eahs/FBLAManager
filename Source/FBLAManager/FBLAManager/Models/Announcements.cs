6061ba72832ff13087a4e060f898f0f5d7176968
Makenna Swartz <makennaswartz03@gmail.com>
2019-10-17 10:42:50 -0400

    Announcements Model

---
+++ Source/FBLAManager/FBLAManager/Models/Announcements.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FBLAManager.Models
+{
+    public class Announcements
+    {
+        public String Title { get; set; }
+        public String Message { get; set; }
+        public String Director { get; set; }
+    }
+}
