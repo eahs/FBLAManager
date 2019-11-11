d16d2a3c237d3269a1dae908e48a2c30bec823cf
Daniel Simboli <dansimboli@gmail.com>
2019-11-11 11:37:42 -0500

    added models that will be necessary in the future

---
+++ Source/FBLAManager/FBLAManager/Models/MeetingAttendees.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace FBLAManager.Models
+{
+    public class MeetingAttendees
+    {
+        public int MemberId { get; set; }
+        public Member Member { get; set; }
+        public int MeetingId { get; set; }
+        public Meeting Meeting { get; set; }
+
+    }
+}
---
+++ Source/FBLAManager/FBLAManager/Models/Member.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace FBLAManager.Models
+{
+    public class Member
+    {
+   
+        public int MemberId { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Password { get; set; }
+
+    }
+
+       
+}
