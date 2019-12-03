ec2de41fccf09d3b99635a3e3980c5155bd9d6dc
Makenna Swartz <makennaswartz03@gmail.com>
2019-12-03 14:14:08 -0800

    Separated About page into About/Terms/Privacy

---
+++ Source/FBLAManager/FBLAManager/Views/TermsPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FBLAManager.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TermsPage : ContentPage
+    {
+        public TermsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+
+        }
+
+        public string Content { get; set; } = "https://eahs.github.io/FBLAManager/TERMS.html?" + Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 8);
+
+    }
+}
