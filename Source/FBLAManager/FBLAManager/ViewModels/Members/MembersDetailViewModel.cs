a6bfa0f6c5041585194bad2266ec262d911c73a3
Daniel Simboli <dansimboli@gmail.com>
2020-01-09 12:59:38 -0500

    added method to send email in member detail page

--- Source/FBLAManager/FBLAManager/ViewModels/Members/MembersDetailViewModel.cs
+++ Source/FBLAManager/FBLAManager/ViewModels/Members/MembersDetailViewModel.cs
@@ -16,6 +16,7 @@ namespace FBLAManager.ViewModels.Events
             Title = member?.FullName;
             Member = member;
         }
+
     }
 }
 
--- Source/FBLAManager/FBLAManager/Views/Members/MembersDetailPage.xaml.cs
+++ Source/FBLAManager/FBLAManager/Views/Members/MembersDetailPage.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using FBLAManager.Models;
 using System;
 using Xamarin.Forms;
-using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
 
 namespace FBLAManager.Views.Members
 {
@@ -18,6 +21,38 @@ namespace FBLAManager.Views.Members
             this.Member = member;
 
             BindingContext = member;
+        }
+
+        public async Task SendEmail()
+        {
+            var recip = new List<string>();
+            recip.Add(Member.Email);
+
+            await SendEmail(string.Empty, string.Empty, recip);
+        }
+
+        public async Task SendEmail(string subject, string body, List<string> recipients)
+        {
+            try
+            {
+                var message = new EmailMessage
+                {
+                    Subject = subject,
+                    Body = body,
+                    To = recipients,
+                    //Cc = ccRecipients,
+                    //Bcc = bccRecipients
+                };
+                await Email.ComposeAsync(message);
+            }
+            catch (FeatureNotSupportedException fbsEx)
+            {
+                // Email is not supported on this device
+            }
+            catch (Exception ex)
+            {
+                // Some other exception occurred
+            }
         }
     }
 }
