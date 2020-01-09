06110a5b4d74f96593cb6692fc6c8905ceef5730
Daniel Simboli <dansimboli@gmail.com>
2020-01-09 12:28:30 -0500

    added twitter feed page

---
+++ Source/FBLAManager/FBLAManager/Views/TwitterFeedPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FBLAManager.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TwitterFeedPage : ContentPage
+    {
+        public TwitterFeedPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        public string ContentURL { get; set; } = "https://twitter.com/FblaManager/lists/fbla-tweets?ref_src=twsrc%5Etfw" + Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 8);
+
+        //public string ContentURL { get; set; } = "http://fblamanager.me/api/twitter" + Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 8);
+
+        //page does not load for some reason
+    }
+}
