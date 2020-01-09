e4bdfb09301650e6c30783ed371da292e749e054
Makenna Swartz <makennaswartz03@gmail.com>
2020-01-09 12:21:14 -0800

    member detail pages show info

--- Source/FBLAManager/FBLAManager/ViewModels/Members/MembersDetailViewModel.cs
+++ Source/FBLAManager/FBLAManager/ViewModels/Members/MembersDetailViewModel.cs
@@ -1,60 +1,21 @@
-﻿using System;
-using System.Threading.Tasks;
-using Xamarin.Essentials;
-using Xamarin.Forms;
-using Xamarin.Forms.Internals;
+﻿using FBLAManager.Models;
 
-namespace FBLAManager.ViewModels.Members
+namespace FBLAManager.ViewModels.Events
 {
     /// <summary>
-    /// ViewModel for profile page
+    /// Viewmodel of meeting detail templates.
     /// </summary>
-    [Preserve(AllMembers = true)]
-    public class MembersDetailViewModel
+
+    //[QueryProperty("Meeting", "meeting")]
+    public class MembersDetailViewModel : BaseViewModel
     {
-        #region Constructor
+        public Member Member { get; set; }
 
-        /// <summary>
-        /// Initializes a new instance for the <see cref="ProfileViewModel" /> class
-        /// </summary>
-        public MembersDetailViewModel()
+        public MembersDetailViewModel(Member member = null)
         {
-            this.TextCommand = new Command(this.TextButtonClicked);
-            this.EmailCommand = new Command(this.EmailClicked);
+            Title = member?.FullName;
+            Member = member;
         }
-
-        #endregion
-
-        #region Command
-
-        /// <summary>
-        /// Gets or sets the command that is executed when the edit button is clicked.
-        /// </summary>
-        public Command TextCommand { get; set; }
-
-        /// <summary>
-        /// Gets or sets the command that is executed when the available status is clicked.
-        /// </summary>
-        public Command EmailCommand { get; set; }
-
-        #endregion
-
-        #region Methods
-
-        /// <summary>
-        /// Invoked when the edit button is clicked.
-        /// </summary>
-        /// <param name="obj">The object</param>
-        private void TextButtonClicked(object obj)
-        {
-            // Do something
-        }
-
-        private void EmailClicked(object obj)
-        {
-            Launcher.TryOpenAsync("makennaswartz03@gmail.com");
-        }
-
-        #endregion
     }
 }
+
