3156f657ed1a660c06dd569b7f97c786ea7d77d9
Daniel Simboli <dansimboli@gmail.com>
2020-01-24 11:21:36 -0500

    added share button to social media

--- Source/FBLAManager/FBLAManager/Views/SocialMedia/AboutUsWithScrollPage.xaml.cs
+++ Source/FBLAManager/FBLAManager/Views/SocialMedia/AboutUsWithScrollPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms.Internals;
+﻿using Xamarin.Essentials;
+using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
 
 namespace FBLAManager.Views.SocialMedia
@@ -14,6 +15,15 @@ namespace FBLAManager.Views.SocialMedia
         public AboutUsWithScrollPage()
         {
             InitializeComponent();
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, System.EventArgs e)
+        {
+            await Share.RequestAsync(new ShareTextRequest
+            {
+                Text = "Check out the new FBLA Navigator app!  It's really useful for FBLA members to learn all about FBLA, take attendance, and stay up to date with the latest announcements!",
+                Title = "FBLA Navigator App"
+            });
         }
     }
 }
