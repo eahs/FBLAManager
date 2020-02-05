0cd4e8d9086526ce0e473274c64c87a6464cb852
Makenna Swartz <makennaswartz03@gmail.com>
2020-02-05 12:45:01 -0800

    Added image editor

---
+++ Source/FBLAManager/FBLAManager/Views/Members/ProfileImageEditor.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FBLAManager.Views.Members
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfileImageEditor : ContentPage
+    {
+        public ProfileImageEditor()
+        {
+            InitializeComponent();
+        }
+
+        public ProfileImageEditor(ImageSource src)
+        {
+            InitializeComponent();
+
+            editor.Source = src;
+        }
+
+    }
+}
