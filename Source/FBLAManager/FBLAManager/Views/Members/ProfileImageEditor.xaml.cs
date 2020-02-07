2f3ab1350a491cf1e7f6c9b284f3c8752b191386
Makenna Swartz <makennaswartz03@gmail.com>
2020-02-07 12:12:24 -0800

    Removed items from toolbar

--- Source/FBLAManager/FBLAManager/Views/Members/ProfileImageEditor.xaml.cs
+++ Source/FBLAManager/FBLAManager/Views/Members/ProfileImageEditor.xaml.cs
@@ -1,5 +1,7 @@
-﻿using Xamarin.Forms;
+﻿using Syncfusion.SfImageEditor.XForms;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Linq;
 
 namespace FBLAManager.Views.Members
 {
@@ -8,14 +10,56 @@ namespace FBLAManager.Views.Members
     {
         public ProfileImageEditor()
         {
-            InitializeComponent();
+            InitializeComponent();    
         }
 
         public ProfileImageEditor(ImageSource src)
         {
             InitializeComponent();
 
+            // The following built-in toolbar item names are available in image editor: 
+            // Back, Text, Add, TextColor, FontFamily, Arial, Noteworthy, Marker Felt, Bradley Hand, 
+            // SignPainter, Opacity, Path, StrokeThickness, Colors, Opacity, Shape, Rectangle, StrokeThickness, Circle, 
+            // Arrow, Transform, Crop, free, original, square, 3:1, 3:2, 4:3, 5:4, 16:9, Rotate, Flip, Reset, Undo, Redo, 
+            // Save, Effects, Hue, Saturation, Brightness, Contrast, Blur and Sharpen.
+            var desiredOptions = new string[] { "Reset", "Effects", "Save", "Undo", "Redo" };
+
             editor.Source = src;
+
+            var toolbarItems = editor.ToolbarSettings.ToolbarItems;
+
+            for (int i = toolbarItems.Count-1; i >= 0; i--)
+            {
+                var item = toolbarItems[i];
+
+                if (!desiredOptions.Contains(item.Name))
+                {
+                    editor.ToolbarSettings.ToolbarItems.Remove(item);
+                }
+            }
+            /*
+            foreach (Syncfusion.SfImageEditor.XForms.ToolbarItem item in editor.ToolbarSettings.ToolbarItems)
+            {
+                if (item.Name == "Text")
+                {
+                    editor.ToolbarSettings.ToolbarItems.Remove(item); 
+                }
+            } */
+
+            /*
+            editor.ToolbarSettings.ToolbarItems.Clear();
+            
+            HeaderToolbarItem undo = new HeaderToolbarItem { Name = "Undo", Icon = ImageSource.FromResource("ImageEditor.undo.png") };
+            HeaderToolbarItem redo = new HeaderToolbarItem { Icon = ImageSource.FromResource("ImageEditor.Redo.png") };
+
+
+            editor.ToolbarSettings.ToolbarItems.Add(undo);
+            editor.ToolbarSettings.ToolbarItems.Add(redo);
+
+            editor.ToolbarSettings.ToolbarItems.Add(new HeaderToolbarItem() { Name = "Undo" } ); */
+
+
+
         }
 
     }
