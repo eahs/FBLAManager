e1327178cf519a5dbe2a429be585e7efdeaf051b
Daniel Simboli <dansimboli@gmail.com>
2020-05-01 16:01:17 -0400

    added WebAuthentication activity

---
+++ Source/FBLAManager/FBLAManager.Android/WebAuthenticationCallbackActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Net;
+using Xamarin.Essentials;
+
+namespace FBLAManager.Droid
+{
+
+    [Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop)]
+    [IntentFilter(new[] { Intent.ActionView },
+        Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
+        DataScheme = CALLBACK_SCHEME)]
+    public class WebAuthenticationCallbackActivity : Xamarin.Essentials.WebAuthenticatorCallbackActivity
+    {
+        const string CALLBACK_SCHEME = "FBLAManager";
+        
+    }
+}
